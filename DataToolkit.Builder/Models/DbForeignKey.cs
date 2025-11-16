748e52a89dc31ab3011a898cb1d12f088a8c6fe8
Fernando <dario.fernando.poveda@gmail.com>
2025-11-16 18:54:59 -0500

    Llaves foraneas!!

---
+++ DataToolkit.Builder/Models/DbForeignKey.cs
@@ -0,0 +1,10 @@
+﻿namespace DataToolkit.Builder.Models;
+
+public class DbForeignKey
+{
+    public string Name { get; set; } = "";
+    public string Column { get; set; } = "";
+    public string ReferencedSchema { get; set; } = "";
+    public string ReferencedTable { get; set; } = "";
+    public string ReferencedColumn { get; set; } = "";
+}
