755778844bcd1d5baf6c4454231d0a2931770d11
Fernando <dario.fernando.poveda@gmail.com>
2026-04-16 22:30:14 -0500

    Compiler

--- DataToolkit.Library/Fluent/Compilation/SqlCompiler.cs
+++ DataToolkit.Library/Fluent/Compilation/SqlCompiler.cs
@@ -2,41 +2,47 @@
 using DataToolkit.Library.Fluent.Sql;
 using System.Text;
 
+namespace DataToolkit.Library.Fluent.Compilation;
+
 internal sealed class SqlCompiler
 {
     public string Compile(FluentQuery q)
     {
-        var sql = new StringBuilder();
+        var sb = new StringBuilder();
 
         foreach (var node in q.Nodes)
         {
-            Render(sql, node);
-            sql.Append(' ');
+            Render(sb, node);
+            sb.Append(' ');
         }
 
-        return sql.ToString().Trim();
+        return sb.ToString().Trim();
     }
 
-    private void Render(StringBuilder sql, SqlNode node)
+    private void Render(StringBuilder sb, SqlNode node)
     {
         switch (node)
         {
             case SqlRaw r:
-                sql.Append(r.Text);
+                sb.Append(r.Text);
                 break;
 
             case SqlBinary b:
-                sql.Append('(');
-                Render(sql, b.Left);
-                sql.Append(' ').Append(b.Op).Append(' ');
-                Render(sql, b.Right);
-                sql.Append(')');
+                sb.Append('(');
+                Render(sb, b.Left);
+                sb.Append(' ').Append(b.Op).Append(' ');
+                Render(sb, b.Right);
+                sb.Append(')');
                 break;
 
             case SqlGroup g:
-                sql.Append('(');
-                Render(sql, g.Node);
-                sql.Append(')');
+                sb.Append('(');
+                Render(sb, g.Node);
+                sb.Append(')');
+                break;
+
+            case SqlParameter p:
+                sb.Append(p.Name);
                 break;
         }
     }
